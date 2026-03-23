6ab3c7c501b2cc8ed2911c596c0fe4891ea4a50e
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2026-03-23 12:17:40 +0900

    add_task

--- simpler_rt1.py
+++ simpler_rt1.py
@@ -29,6 +29,7 @@ task_names = [
     "google_robot_move_near",
     "google_robot_open_drawer",
     "google_robot_close_drawer",
+    "google_robot_place_in_closed_drawer",
     "widowx_spoon_on_towel",
     "widowx_carrot_on_plate",
     "widowx_stack_cube",
